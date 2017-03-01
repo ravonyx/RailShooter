774f6f5debad56da96711be9cd156ce494669d82
DESKTOP-JVPO5O1\Ravonyx <ravonyxa@gmail.com>
2017-03-01 20:34:28 +0100

    first commit (mainMenu + mainScene created)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.VR;
+
+public class CameraController : MonoBehaviour
+{
+    private int _yMinLimit = -40;
+    private int _yMaxLimit = 50;
+
+    private Vector2 _cameraRotation;
+
+    [SerializeField]
+    public bool isActive;
+    [SerializeField]
+    public float sensitivity;
+
+    void Start()
+    {
+       // if (!Application.isEditor)
+        UnityEngine.Cursor.visible = false;
+        isActive = true;
+        _cameraRotation = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!isActive)
+            return;
+
+        float y = -Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        float x = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+
+        _cameraRotation.y += y;
+        _cameraRotation.x += x;
+        _cameraRotation.y = ClampAngle(_cameraRotation.y, _yMinLimit, _yMaxLimit);
+        transform.localRotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0);
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Clamp(angle, min, max);
+    }
+
+    float Clamp(float value, float min, float max)
+    {
+        if (value < min)
+            value = min;
+        if (value > max)
+            value = max;
+        return value;
+    }
+}

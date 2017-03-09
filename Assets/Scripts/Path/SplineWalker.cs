5f8d44906966544fecc82de1a5078bd5df740bf1
Manon Maillard <ravonyxa@gmail.com>
2017-03-09 17:06:03 +0100

    spline stop

--- Assets/Scripts/Path/SplineWalker.cs
+++ Assets/Scripts/Path/SplineWalker.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
-public class SplineWalker : MonoBehaviour {
+public class SplineWalker : MonoBehaviour
+{
 
 	public BezierSpline spline;
 	public float duration;
 	public bool lookForward;
 	private float progress;
+
+    public List<float> stopPoints;
+    void Start()
+    {
+        stopPoints = spline.stopPoints;
+    }
 
 	private void Update ()
     {
@@ -14,6 +22,13 @@ public class SplineWalker : MonoBehaviour {
 				progress = 1f;
 
 		Vector3 position = spline.GetPoint(progress);
+
+        Debug.Log(position);
+        Debug.Log(spline.GetPoint(stopPoints[0]));
+        if (position == spline.GetPoint(stopPoints[0]))
+        {
+            Debug.Log("Stop");
+        }
 		transform.localPosition = position;
 		if (lookForward)
 			transform.LookAt(position + spline.GetDirection(progress));

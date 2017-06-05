b07b91946ea9c6d8f8049eeb356dd8e8dd0b635c
OCULUS-PC\oculus <ravonyxa@gmail.com>
2017-06-05 22:35:26 +0200

    Oculus touch updgrade

---
+++ Assets/Scripts/Cameras/VRManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_leftHandAnchor;
+    [SerializeField]
+    private Transform m_rightHandAnchor;
+
+    void Start ()
+    {
+        UpdateAnchors();
+    }
+
+    void Update ()
+    {
+        OVRInput.Update();
+        UpdateAnchors();
+    }
+
+    private void UpdateAnchors()
+    {
+        m_leftHandAnchor.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        m_rightHandAnchor.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+
+        m_leftHandAnchor.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        m_rightHandAnchor.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+    }
+}

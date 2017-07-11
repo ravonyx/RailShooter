591ba27c9ec9e0d5674e2d15ce42f885a0fa8587
OCULUS-PC\oculus <ravonyxa@gmail.com>
2017-07-11 23:25:20 +0200

    fade panel

--- Assets/Scripts/Menu/MenuButton.cs
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -14,28 +14,28 @@ namespace RailShooter.Menu
     {
         public event Action<MenuButton> OnButtonSelected;                   // This event is triggered when the selection of the button has finished.
 
-
+        [SerializeField]
+        private CamerasAndInputsManager m_camInputManager;
         [SerializeField] private string m_SceneToLoad;                      // The name of the scene to load.
-        [SerializeField] private CameraFade m_CameraFade;                 // This fades the scene out when a new scene is about to be loaded.
-        [SerializeField] private SelectionRadial m_SelectionRadialVR;         // This controls when the selection is complete.
-        [SerializeField] private SelectionRadial m_SelectionRadialMouse;         // This controls when the selection is complete.
+        private SelectionRadial m_selectionRadial;        
         [SerializeField] private InteractiveItem m_InteractiveItem;       // The interactive item for where the user should click to load the level.
 
-
+        private CameraFade m_cameraFade;
         private bool m_GazeOver;                                            
-
 
         void Start()
         {
-            m_CameraFade = Camera.main.GetComponent<CameraFade>();
+            m_cameraFade = m_camInputManager.CurrentCamera.GetComponent<CameraFade>();
+            Debug.Log(m_camInputManager.CurrentCamera);
         }
 
         private void OnEnable ()
         {
+            Debug.Log(m_camInputManager.CurrentCamera);
+            m_selectionRadial = m_camInputManager.CurrentCamera.GetComponent<SelectionRadial>();
             m_InteractiveItem.OnOver += HandleOver;
             m_InteractiveItem.OnOut += HandleOut;
-            m_SelectionRadialVR.OnSelectionComplete += HandleSelectionComplete;
-            m_SelectionRadialMouse.OnSelectionComplete += HandleSelectionComplete;
+            m_selectionRadial.OnSelectionComplete += HandleSelectionComplete;
         }
 
 
@@ -43,16 +43,14 @@ namespace RailShooter.Menu
         {
             m_InteractiveItem.OnOver -= HandleOver;
             m_InteractiveItem.OnOut -= HandleOut;
-            m_SelectionRadialVR.OnSelectionComplete -= HandleSelectionComplete;
-            m_SelectionRadialMouse.OnSelectionComplete -= HandleSelectionComplete;
+            m_selectionRadial.OnSelectionComplete -= HandleSelectionComplete;
         }
 
 
         private void HandleOver()
         {
             // When the user looks at the rendering of the scene, show the radial.
-            m_SelectionRadialVR.Show();
-            m_SelectionRadialMouse.Show();
+            m_selectionRadial.Show();
             m_GazeOver = true;
         }
 
@@ -60,9 +58,7 @@ namespace RailShooter.Menu
         private void HandleOut()
         {
             // When the user looks away from the rendering of the scene, hide the radial.
-            m_SelectionRadialVR.Hide();
-            m_SelectionRadialMouse.Hide();
-
+            m_selectionRadial.Hide();
             m_GazeOver = false;
         }
 
@@ -77,7 +73,7 @@ namespace RailShooter.Menu
         private IEnumerator ActivateButton()
         {
             // If the camera is already fading, ignore.
-            if (m_CameraFade.IsFading)
+            if (m_cameraFade.IsFading)
                 yield break;
 
             // If anything is subscribed to the OnButtonSelected event, call it.
@@ -85,7 +81,7 @@ namespace RailShooter.Menu
                 OnButtonSelected(this);
 
             // Wait for the camera to fade out.
-            yield return StartCoroutine(m_CameraFade.BeginFadeOut(true));
+            yield return StartCoroutine(m_cameraFade.BeginFadeOut(true));
 
             // Load the level.
             SceneManager.LoadScene(m_SceneToLoad, LoadSceneMode.Single);

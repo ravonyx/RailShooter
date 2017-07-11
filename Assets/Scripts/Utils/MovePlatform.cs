8c4c23969f9a7220f544fd2f538cd911b1dfa959
DESKTOP-JVPO5O1\Ravonyx <ravonyxa@gmail.com>
2017-07-11 12:24:19 +0200

    platform in menu and tuto

---
+++ Assets/Scripts/Utils/MovePlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RailShooter.Utils;
+
+public class MovePlatform : MonoBehaviour
+{
+    [SerializeField]
+    CamerasAndInputsManager m_camInputManager;
+    [SerializeField]
+    float m_deltaY;
+
+    private void LateUpdate()
+    {
+        Quaternion quat = Quaternion.Euler(180, 0, 0);
+        Quaternion newRotation = m_camInputManager.CurrentCamera.transform.parent.transform.rotation * quat;
+
+        transform.rotation = newRotation;
+
+        Vector3 camPos = m_camInputManager.CurrentCamera.transform.position;
+        Vector3 newPosition = m_camInputManager.CurrentCamera.transform.parent.transform.position + m_camInputManager.CurrentCamera.transform.parent.transform.up * -m_deltaY;
+
+        transform.position = newPosition;
+    }
+}
